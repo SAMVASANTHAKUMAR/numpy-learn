a032cc933318e0425d4726d93e18151379146a4a
SAMVASANTHAKUMAR <samsvk2005@gmail.com>
2025-07-17 19:29:24 +0530

    Indexingarray

---
+++ Indexingarray.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+# 1D indexing
+arraya = np.array([1, 2, 3, 4])
+print("1D:", arraya[2])  # Expected: 3
+
+# 2D indexing 
+arrayb = np.array([[1, 2], [3, 4], [5, 6]])
+print("2D:", arrayb[1, 1])  # Expected: 4
+
+# 3D indexing
+arrayc = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print("3D:", arrayc[0, 1, 1])  # Expected: 4
+
+# Negative indexing
+print("Negative:", arrayb[-1])
+input()
