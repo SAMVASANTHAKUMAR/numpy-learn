2f61ac35001437777a33b18c8267658438f51608
SAMVASANTHAKUMAR <samsvk2005@gmail.com>
2025-07-17 18:59:57 +0530

    Created array numpy

---
+++ creatingarray.py
@@ -0,0 +1,13 @@
+import numpy as np 
+#1D array
+array1=np.array([1,2,3,4,5])
+print("1D array = ",array1)
+#2D array
+array2=np.array([[1,2],[3,4]])
+print("2D array = ",array2)
+#3D array
+array3=np.array([[[1,2],[3,4],[5,6]]])
+print("3D array = ",array3)
+#multiDimensional array
+arraym=np.array([1,2,3,4],ndmin=5)
+print("MultiD array = ",arraym)
