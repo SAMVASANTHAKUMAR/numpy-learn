a2b74803219b2228435d1c556cac9b51f21bcbaa
SAMVASANTHAKUMAR <samsvk2005@gmail.com>
2025-07-17 22:14:46 +0530

    Concatenate Stack and Split

--- ConcatStackandSplit.py
+++ ConcatStackandSplit.py
@@ -9,6 +9,20 @@ arrayd=np.array([[5,6],[7,8]])
 print("Concatenated 2D array =",np.concatenate((arrayc,arrayd),axis=1))
 print()
 
+#stacking
+#hstack() horizontal stacking
+arrayhstack=np.hstack((arrayc,arrayd))
+print("H stacked array =",arrayhstack)
+print()
+#vstack() vertical stacking
+arrayvstack=np.vstack((arrayc,arrayd))
+print("V stacked array =",arrayvstack)
+print()
+#dstack depth of array
+arraydstack=np.dstack((arrayc,arrayd))
+print("D stack = ",arraydstack)
+print()
+
 #Splitting array
 arraye=np.array([1,2,3,4,5,6,7,8,9])
 newarr=np.array_split(arraye,5)
