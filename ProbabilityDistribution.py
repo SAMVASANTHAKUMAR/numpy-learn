35755496192a11f550995c8333f7d5590141d6cc
SAMVASANTHAKUMAR <samsvk2005@gmail.com>
2025-07-18 20:48:23 +0530

    ProbabilityDistribution

--- ProbabilityDistribution.py
+++ ProbabilityDistribution.py
@@ -2,5 +2,6 @@ import numpy as np
 from numpy import random
 #in choice() p as a parameter,we can give probabilities of each element to be appeared
 #ex: element with high probability appears more than element with less probability
+#sum of all probabilities should be 1 and given probabilities should be within 0-1
 x=random.choice([1,2,3,4,5,],p=[0.2,0.2,0.2,0.3,0.1],size=(5))
 print("Choice with probablity specified = ",x)
